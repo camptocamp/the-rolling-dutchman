b8408d75299d1b3a10120e323b2a7d7e28470b94
Kalaboum <nataniel.hofer@gmail.com>
2018-05-03 10:33:27 +0200

    adding unit tests, still can't debug

--- test/cutting_shapes_test.js
+++ test/cutting_shapes_test.js
@@ -1,6 +1,10 @@
 /* eslint-disable */
+<<<<<<< HEAD
 import 'babel-polyfill'
 import {makeKey, createFragmentsForStopTimes, FractionedShapeDict, FragmentedTrip} from '../src/modules/cutting_shapes';
+=======
+import {makeKey, createFragmentsForStopTimes} from '../src/modules/cutting_shapes';
+>>>>>>> 078a38c... adding unit tests, still can't debug
 
 const assert = require('assert');
 
@@ -21,8 +25,13 @@ function mockUpStopTime(departure_time, arrival_time, shape_dist_traveled) {
 
 //TODO finish this
 describe('createFragmentsForStopTimes', () => {
+<<<<<<< HEAD
   const shapeDict1 = new FractionedShapeDict();
   const shapeDict2 = new FractionedShapeDict();
+=======
+  const shapeDict1 = {};
+  const shapeDict2 = {};
+>>>>>>> 078a38c... adding unit tests, still can't debug
   const hour1 = '08:11:00';
   const hour2 = '08:13:00';
   const hour3 = '12:15:00';
@@ -31,6 +40,7 @@ describe('createFragmentsForStopTimes', () => {
     mockUpStopTime(hour2, hour2, 1), mockUpStopTime(unusedHour, hour3, 3)];
   const stopTimes2 = [mockUpStopTime(hour1, unusedHour, 0), mockUpStopTime(unusedHour, hour2, 3)];
   const shapeDist = [0, 1, 3];
+<<<<<<< HEAD
   const firstResult = {"01": [{startTime: hour1, endTime: hour2}], "12": [{startTime: hour2, endTime: hour3}]};
   const secondResult = {"02": [{startTime: hour1, endTime: hour2}]};
   console.log(shapeDict1);
@@ -63,3 +73,19 @@ describe('the method addTrip', () => {
     assert.equal(fractionedShapeDict.innerDictionary[key].length, 2)
   })
 })
+=======
+  const firstResult = {"01": {st: hour1, et: hour2}, "12": {st: hour2, et: hour3}};
+  const secondResult = {"02": {st: hour1, et: hour2}};
+  const object = createFragmentsForStopTimes(shapeDict1, shapeDist, stopTimes1);
+  it('should not return undefined', () => {
+    assert.notEqual(createFragmentsForStopTimes(shapeDict1, shapeDist, stopTimes1), undefined);
+  })
+  it('should split a shape according to the shape_dist_traveled', () => {
+    assert.deepEqual(createFragmentsForStopTimes(shapeDict1, shapeDist, stopTimes1), firstResult);
+  });
+
+  it('should not split the trip for an express', () => {
+    assert.deepEqual(createFragmentsForStopTimes(shapeDict2, shapeDist, stopTimes2), secondResult);
+  });
+});
+>>>>>>> 078a38c... adding unit tests, still can't debug
